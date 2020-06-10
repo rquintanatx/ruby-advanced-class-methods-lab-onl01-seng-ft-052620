a39bc60b799c326829f58589b60de92fa2a0febf
Robert Quintana <robertquintanatx@gmail.com>
2020-06-10 01:38:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -55,6 +55,15 @@ class Song
     song
   end
   
+  def self.create_from_filename(filename)
+    split_file = filename.split(/\.|\-|!/)
+    song = self.new
+    song.name = split_file[1].split.join(" ")
+    song.artist_name = split_file[0].split.join("")
+    song.save
+    song
+  end
+  
   
     
   
